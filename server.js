a246d1cdad34cdd1ab57e6fedfbddbd5f786a892
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-03-28 22:35:30 -0700

    files are created and codes are added

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
+
+app.get('/index', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+require("./routes/routes")(app);
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
