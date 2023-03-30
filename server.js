0f9c97e07de2fb8e072a20143b915f88d5c2c781
fdgardon <119641606+fdgardon@users.noreply.github.com>
2023-03-30 16:32:15 -0700

    codes are updated

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const path = require('path');
 const app = express();
 
 const PORT = 3001;
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Navigate to /index or /notes'));
